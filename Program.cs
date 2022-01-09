222f28274d4e45cfc460e2b8361d74502d2bd99f
teolord <74703619+teolord@users.noreply.github.com>
2022-01-09 13:42:05 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,79 @@
+using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random r = new Random();
+
+            while (true)
+            {
+                int cislo = r.Next(0, 3);
+
+                Console.WriteLine("0 je kamen");
+                Console.WriteLine("V jsou nůžky");
+                Console.WriteLine("- je papír");
+
+                Console.Write("Zadej znak: ");
+                char t = Char.Parse(Console.ReadLine());
+
+                if (t == '#')
+                {
+                    if (cislo == 0)
+                    {
+                        Console.WriteLine("Remíza počitač použil kámen");
+                    }
+
+                    else if (cislo == 1)
+                    {
+                        Console.WriteLine("Vyhral jsi počítač použil nůžky");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Prohrál jsi počítač použil papír");
+                    }
+                }
+
+                else if (t == 'V')
+                {
+                    if (cislo == 0)
+                    {
+                        Console.WriteLine("Prohral jsi pocitac pouzil kamen");
+                    }
+
+                    else if (cislo == 1)
+                    {
+                        Console.WriteLine("Remizoval jsi počítač použil nůžky");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Vyhral jsi počítač použil papír");
+                    }
+                }
+
+                else if (t == '-')
+                {
+                    if (cislo == 0)
+                    {
+                        Console.WriteLine("Vyhral jsi počitač použil kámen");
+                    }
+
+                    else if (cislo == 1)
+                    {
+                        Console.WriteLine("Prohral jsi počítač použil nůžky");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Remiza počítač použil papír");
+                    }
+                }
+            }
+        }
+    }
+}
